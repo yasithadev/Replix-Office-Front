86d80d67d21d8a71347ac39846b6a25c0ad38195
Your Name <yasitha.dev@gmail.com>
2024-12-30 00:53:50 +0530

    rename file not using by adding x at the end.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Layout from "./pages/Layout";
+//import Layout from "./pages/Layout";
 import Home from "./pages/Home";
 import Blogs from "./pages/Blogs";
 import Contact from "./pages/Contact";
