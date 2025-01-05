e731afac4c17699c31c8ed37945323f348209896
Your Name <yasitha.dev@gmail.com>
2025-01-05 17:09:56 +0530

    sccessfull signin request using axios

--- src/repositry/authRepository.js
+++ src/repositry/authRepository.js
@@ -1,3 +1,4 @@
+import axios from 'axios';
 class AuthRepository{
 
     constructor(){
@@ -8,7 +9,20 @@ class AuthRepository{
     doBasicAuthentication(username,password){
         console.log(username,"xxxxxxxx");
         console.log(password);
-        
+        let url = 'http://localhost:8080/api/test/signin';
+        axios.post(url,{
+            username: username,
+            password: password
+          })
+          .then(function (response) {
+            console.log(response);
+          })
+          .catch(function (error) {
+            console.log(error);
+          })
+          .finally(function () {
+            // always executed
+          });
         return null;
     }
 }
