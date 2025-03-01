90edc684694ad582b69b78cd22cb88abff9613b7
Your Name <yasitha.dev@gmail.com>
2025-03-01 18:39:43 +0530

    disappear notifications

--- src/components/notification/notification.js
+++ src/components/notification/notification.js
@@ -1,6 +1,20 @@
 import NotificationStyle from './notification.module.css';
+import React, {useState,useRef, useEffect } from 'react';
 
 const Notification = (props) => {  
-    return (<div className={NotificationStyle.notification}>{props.children}</div>);
+    let firstUseEffect = true;
+    const [opacity,setOpacity] = useState(1);
+    useEffect(() => {
+        if(firstUseEffect){
+            setTimeout(selfRemove, 3000);
+        }
+        firstUseEffect = false;
+    }, [props]);
+    const selfRemove = () => {  
+        console.log("selfRemove");
+        setOpacity(0);
+        setTimeout(props.remove, 300);
+    }
+    return (<div className={NotificationStyle.notification} style={{opacity: opacity}}>{props.children}</div>);
 }
 export default Notification;
--- src/components/notification/notificationContainer.js
+++ src/components/notification/notificationContainer.js
@@ -3,17 +3,25 @@ import NotificationContainerStyle from './notificationContainer.module.css';
 import Notification from './notification';
 
 const NotificationContainer = (props) => { 
-    const [notifications, setNotifications] = useState(["Error"]);
+    let notificationsWorkingCopy = ["Error"];
+    const [notifications, setNotifications] = useState(notificationsWorkingCopy);
+    
     useEffect(() => {
         document.addEventListener('notify', notyfy, false); 
     }, [props]);
     const notyfy=(e)=> {  
         console.log("<----------------------- notify =============================>");
-        setNotifications(["warning","Error"])
+        notificationsWorkingCopy=["warning","Error"];
+        setNotifications(notificationsWorkingCopy);
         //document.dispatchEvent(new Event("notify"));
     }
+    const removeLast=()=> {
+        notificationsWorkingCopy.pop();
+        setNotifications(notificationsWorkingCopy);
+        console.log("notificationsWorkingCopy ",notificationsWorkingCopy.toString);
+    }
     return(<div className={NotificationContainerStyle.notificationContainer }>
-    {notifications.map(notification=>(<Notification key={notification}>{notification}</Notification>))}
+    {notifications.map(notification=>(<Notification key={notification}remove={removeLast}>{notification }</Notification>))}
     </div>);
 }
 export default NotificationContainer;
